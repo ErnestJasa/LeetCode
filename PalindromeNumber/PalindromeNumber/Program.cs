4054b39fe5f669514012aa760c95870a98cfeb43
ErnestJasa <ernestjasaitis@gmail.com>
2023-05-05 21:01:01 +0300

    Palindrome number

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+int num = 121;
+bool IsPalindrome(int x)
+{
+    int reminder;
+    int revNumber = 0;
+    int temp = x;
+
+    while (x > 0)
+    {
+        reminder = x % 10;
+        x = x / 10;
+        revNumber = (revNumber * 10) + reminder;
+
+    }
+    if (revNumber == temp)
+    {
+        return true;
+    }
+    else
+    {
+
+        return false;
+    }
+}
+
+Console.WriteLine(IsPalindrome(num));
