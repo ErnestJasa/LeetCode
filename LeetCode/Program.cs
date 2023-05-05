7c2e683b49497b95085fb454012c41efc271def0
ErnestJasa <ernestjasaitis@gmail.com>
2023-05-05 20:54:08 +0300

    first

---
+++ LeetCode/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] arr = new int[] { 3, 2, 3 };
+
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        for (int j = i + 1; j <= nums.Length - 1; j++)
+        {
+
+            int temp = nums[i] + nums[j];
+            if (temp == target)
+            {
+                return new int[] { i, j };
+            }
+        }
+    }
+    return new int[] { -1 };
+}
+
+
+foreach (var item in TwoSum(arr, 6))
+{
+    Console.WriteLine(item);
+}
