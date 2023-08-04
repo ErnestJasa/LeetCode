5df14d218029ff6bbb1c4953f22337436f863b14
ErnestJasa <ernestjasaitis@gmail.com>
2023-08-04 12:53:28 +0300

    valid parantheses

---
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,26 @@
+﻿
+bool IsValid(string s)
+{
+    while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+    {
+        s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+    }
+
+    return s.Length == 0;
+}
+
+
+
+
+
+string test = "{[]}}";
+
+
+if (test.Contains("[]"))
+{
+    test = test.Replace("[]", "");
+    Console.WriteLine(test);
+    Console.WriteLine(test.Contains("{}"));
+}
+
+Console.WriteLine(IsValid(test));
