09b31caf438e84a63133d497386d9a5cf3408614
ErnestJasa <ernestjasaitis@gmail.com>
2023-08-14 12:23:58 +0300

    remove unnecessary lines

--- RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
@@ -7,15 +7,12 @@ int[] nums = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 int RemoveDuplicates2(int[] arr)
 {
     int lengthWithoutDuplicates = 1;
-    int i = 0;
     foreach (int n in nums)
     {
         if (nums[lengthWithoutDuplicates - 1] != n)
         {
-            nums[lengthWithoutDuplicates++] = n;
-            arr[i] = 0;
+            nums[lengthWithoutDuplicates++] = n;            
         }
-        i++;
     }
     return lengthWithoutDuplicates;
 }
