cb51db17c2013a36c58294150279c89739dd25ab
ErnestJasa <ernestjasaitis@gmail.com>
2023-08-14 13:04:08 +0300

    FindTheIndexOfTheFirstOccurrenceInString

---
+++ FindTheIndexOfTheFirstOccurrenceInString/FindTheIndexOfTheFirstOccurrenceInString/Program.cs
@@ -0,0 +1,26 @@
+﻿string haystack = "hkjasdfkiykiuytasdfkiyki";
+
+string needle = "kiyki";
+
+int ReturnIndex(string haystack, string needle)
+{
+
+    if (haystack.Contains(needle))
+    {
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i..(needle.Length + i)] == needle)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+Console.WriteLine(ReturnIndex(haystack, needle));
+
+Console.WriteLine(haystack.IndexOf(needle));
