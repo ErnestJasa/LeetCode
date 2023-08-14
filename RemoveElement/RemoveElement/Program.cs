988870ea6ae1ba406c44d39b5c5dd4ae15203ef3
ErnestJasa <ernestjasaitis@gmail.com>
2023-08-14 12:37:19 +0300

    remove element

---
+++ RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int[] array = new[] { 1, 2, 3, 3, 3, 4 };
+
+int Remove(int[] arr, int valueToRemove)
+{
+    int current = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != valueToRemove)
+        {
+            arr[current] = arr[i];
+            current++;
+        }
+    }
+   
+   
+    return current;
+}
+
+Console.WriteLine(Remove(array, 3));
