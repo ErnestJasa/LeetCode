3fcf768eb45cd9e752a97d8b9fa98fe41fa8bfed
ErnestJasa <ernestjasaitis@gmail.com>
2023-08-16 13:39:35 +0300

    plus one

---
+++ PlusOne/PlusOne/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int[] arr = new[] { 1,2, 3, 4 };
+
+int[] PlusOne(int[] arr)
+{
+    for (int i = arr.Length - 1; i >= 0; i--)
+    {
+        if (i == 0 && arr[0] == 9)
+        {
+            int[] newArray = new int[arr.Length+1];
+            newArray[0] = 1;
+            
+            return newArray;
+        }
+        if (arr[i] != 9)
+        {
+            arr[i] += 1;
+            break;
+        }
+        else if (arr[i] == 9)
+        {
+            arr[i] = 0;
+        }
+        else
+        {
+            arr[i] += 1;
+            break;
+        }
+    }
+
+    return arr;
+}
+
+Console.WriteLine(PlusOne(arr));
